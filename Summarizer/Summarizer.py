45f106f6ae10bad8a96a77bf84320cf8f850f9ff
Magnus Steinsvoll Prøsch <mp@fsncapital.com>
2019-01-11 15:07:59 +0100

    adjustment to length

--- Summarizer/Summarizer.py
+++ Summarizer/Summarizer.py
@@ -44,7 +44,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     #parser.add_argument('filepath', help='File name of text to summarize', required='False')
     parser.add_argument('url', help='Url of text to summarize')
-    parser.add_argument('-l', '--length', default=10, help='Number of sentences to return')
+    parser.add_argument('-l', '--length', default=4, help='Number of sentences to return')
     args = parser.parse_args()
 
     return args
